0787ac127e304d5d1c166a154d56f6745e9c82bf
河合彰紘 <rakko1124@gmail.com>
2020-12-06 07:01:35 +0900

    作業時間　→ 約３時間くらい
    ユーザー登録フォーム整えた。
    コントローラーcreateまで完了。
    実際のページでも動作確認済み
    rails tutorials 的には７章までできた。

--- app/controllers/masters_controller.rb
+++ app/controllers/masters_controller.rb
@@ -1,5 +1,26 @@
 class MastersController < ApplicationController
+
   def new
     @master = Master.new
   end
+
+  def create
+    @master = Master.new(master_params)
+    if @master.save
+      flash[:success] = "ユーザー登録が完了しました！次はシフト時間帯の設定をしていきましょう！"
+      redirect_to @master
+      #最終的にはsettingページに飛ばしたい。
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @master = Master.find(params[:id])
+  end
+
+  private
+    def master_params
+      params.require(:master).permit(:store_name, :user_name, :password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
-  get '/help', to: 'static_pages#help'
-  get  '/signup', to: 'masters#new'
+  get '/help',   to: 'static_pages#help'
+  get  '/signup',  to: 'masters#new'
   resources :masters
 end
