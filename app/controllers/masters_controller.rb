0855c43be1db78af9af376da60838960125e209a
河合彰紘 <rakko1124@gmail.com>
2020-12-12 11:45:55 +0900

    1時間30分
    master editフォーム、patch、侵入権限の追加をした
    
    destroyは今回のmasterには必要ないので省くことにした。

--- app/controllers/masters_controller.rb
+++ app/controllers/masters_controller.rb
@@ -1,4 +1,6 @@
 class MastersController < ApplicationController
+  before_action :logged_in_master, only: [:edit, :update]
+  before_action :corrent_master, only: [:edit, :update]
 
   def new
     @master = Master.new
@@ -24,8 +26,30 @@ class MastersController < ApplicationController
     @master = Master.find(params[:id])
   end
 
+  def update
+    @master = Master.find(params[:id])
+    if @master.update(master_params)
+      flash[:success] = "プロフィールを変更しました"
+      redirect_to @master
+    else
+      render 'edit'
+    end
+  end
+
   private
     def master_params
       params.require(:master).permit(:store_name, :user_name, :password, :password_confirmation)
     end
+
+    def logged_in_master
+      unless logged_in?
+        flash[:danger] = "Please log in"
+        redirect_to login_url
+      end
+    end
+
+    def corrent_master
+      @master = Master.find(params[:id])
+      redirect_to(root_url) unless current_master?(@master)
+    end 
 end
--- spec/factories/masters.rb
+++ spec/factories/masters.rb
@@ -6,4 +6,10 @@ FactoryBot.define do
     password_confirmation { "foobar" }
   end
 
+  factory :master2, class: Master do
+    sequence(:store_name) {  |n| "My#{n}String2" }
+    user_name { "MyString" }
+    password { "foobar" }
+    password_confirmation { "foobar" }
+  end
 end
