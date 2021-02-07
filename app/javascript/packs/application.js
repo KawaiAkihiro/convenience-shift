504e13f3e331003bd141512a023953f358fce875
KawaiAkihiro <rakko1124@gmail.com>
2021-02-08 07:06:55 +0900

    [add] typescript react 導入(動作確認済み 単体はok 掛け合わせが(?)って感じ) [add] sidebar を追加(css, js)

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -23,3 +23,18 @@ import "../stylesheets/application.scss";
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
+// Support component names relative to this directory:
+var componentRequireContext = require.context("components", true);
+var ReactRailsUJS = require("react_ujs");
+ReactRailsUJS.useContext(componentRequireContext);
+
+$(function(){
+    $("#open").click(function(){
+            $("#menu").css("right","0px");   
+    })
+
+    $("#close").click(function(){
+            $("#menu").css("right","-340px")
+    })
+
+})  
